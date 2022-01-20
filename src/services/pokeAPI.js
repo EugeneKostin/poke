879da47f7c64ea96c841c65d0e06ccb02a635d1d
Eugene <zh.kostin97@gmail.com>
2022-01-20 18:05:48 +0300

    add error return

--- src/services/pokeAPI.js
+++ src/services/pokeAPI.js
@@ -1,23 +1,16 @@
 import axios from "axios";
 
-// export const pokeAPI = (url) => {
-//   axios
-//     .get(url)
-//     .then(response => {
-//       console.log("response", response);
-//       return response;
-//     })
-//     .catch(error => {
-//       console.log("error", error);
-//     });
-//   // error Error: Not Found
-// };
-
 export const pokeAPI = async (url) => {
   try {
     const response = await axios.get(url);
-    return response.data;
-  } catch (error) {
-    console.error(error);
+    return { data: response.data };
+  } catch (err) {
+    if (err.response) {
+      return { err: '404 Not Found' }
+    } else if (err.request) {
+      return { err: 'Ошибка сети' }
+    } else {
+      return { err: 'Ошибка приложения' }
+    }
   }
 }
